f0e8b70455432343b5b913257a5c0fb9821d7178
Rafael Bittar <rafaelmbittar@gmail.com>
2025-10-17 17:24:30 -0300

    Updating fields and layout of the form

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,12 +31,66 @@ function createTextArea(item_name, label_description) {
 	return new_item;
 }
 
+function createItemSection(section_name, section_title){
+	let item_section = document.createElement("div");
+	let item_title = document.createElement("h2");
+
+	item_title.textContent = section_title;
+
+	item_section.classList.add(section_name, "item_section");
+
+	item_section.appendChild(item_title);
+
+	return item_section;
+}
+
 function addItem() {
 	const new_item = document.createElement("div");
 	const pedido_div = document.getElementById("pedido");
 
+	new_item.classList.add("item_pedido");
+
+	const compra_section = createItemSection("compra", "Compra do item");
+	const gravacao_section = createItemSection("gravacao", "Gravação");
+	const comissao_section = createItemSection("comissao", "Comissão");
+	const lucro_section = createItemSection("lucro", "Lucro");
+	const bv_section = createItemSection("bv", "BV");
+	const venda_section = createItemSection("venda", "Venda");
+
 	// CREATING THE FORM INPUTS FOR THE NEW ITEM
-	new_item.appendChild(createInput("vlr_un", "number", "Valor unitário"));
+	// -=-=-=-=-=-=- COMPRA DO ITEM -=-=-=-=-=-=-
+	new_item.appendChild(compra_section);
+	compra_section.appendChild(createInput("fornecedor", "text", "Fornecedor"));
+	compra_section.appendChild(createInput("codigo", "text", "Código de referência"));
+	compra_section.appendChild(createInput("item", "text", "Item"));
+	compra_section.appendChild(createTextArea("descricao", "Descrição"));
+	compra_section.appendChild(createInput("peso", "number", "Peso"));
+	compra_section.appendChild(createInput("formato", "text", "Formato"));
+	compra_section.appendChild(createInput("qtd_compra", "number", "Quantidade comprada"));
+	compra_section.appendChild(createInput("vlr_un_compra", "number", "Valor unitário - compra"));
+
+	// -=-=-=-=-=-=- GRAVAÇÃO -=-=-=-=-=-=- 
+	new_item.appendChild(gravacao_section);
+	gravacao_section.appendChild(createInput("fornecedor", "text", "Fornecedor"));
+	gravacao_section.appendChild(createInput("tipo", "text", "Tipo"));
+	gravacao_section.appendChild(createInput("qtd_gravacao", "number", "Quantidade para gravação"));
+	gravacao_section.appendChild(createInput("vlr_un_gravacao", "number", "Valor unitário - gravação"));
+
+	// -=-=-=-=-=-=- COMISSÃO -=-=-=-=-=-=-
+	new_item.appendChild(comissao_section);
+	comissao_section.appendChild(createInput("margem_comissao", "number", "Margem de comissão"));
+
+	// -=-=-=-=-=-=- LUCRO -=-=-=-=-=-=-
+	new_item.appendChild(lucro_section);
+	lucro_section.appendChild(createInput("margem_lucro", "number", "Margem de lucro"));
+
+	// -=-=-=-=-=-=- BV -=-=-=-=-=-=-
+	new_item.appendChild(bv_section);
+	bv_section.appendChild(createInput("margem_bv", "number", "Percentual BV"));
+
+	// -=-=-=-=-=-=- VENDA -=-=-=-=-=-=-
+	new_item.appendChild(venda_section);
+	venda_section.appendChild(createInput("qtd_venda", "number", "Quantidade vendida"));
 
 	pedido_div.appendChild(new_item);
 }
--- pedido.php
+++ pedido.php
@@ -12,7 +12,7 @@ get_header();
     <form id="orcamento_form">
     <div id="info">
         <div>
-            <label class="item_field">
+            <label class="order_field">
                 Cliente
                 <input type="text" name="cliente"></input>
             </label>
@@ -20,28 +20,28 @@ get_header();
         </div>
 
         <div>
-            <label class="item_field">
+            <label class="order_field">
                 Solicitante
                 <input type="text" name="solicitante"></input>
             </label>
         </div>
 
         <div>
-            <label class="item_field">
+            <label class="order_field">
                 CNPJ
                 <input type="text" name="cnpj"></input>
             </label>
         </div>
 
         <div>
-            <label class="item_field">
+            <label class="order_field">
                 Data de entrega
                 <input type="datetime-local" name="date_delivery"></input>
             </label>
         </div>
 
         <div>
-            <label class="item_field">
+            <label class="order_field">
                 CEP
                 <input type="text" name="cep"></input>
             </label>
